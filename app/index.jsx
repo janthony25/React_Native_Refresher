aa1e4dd623ce876d2249612a27418557fc0310bd
Jun Anthony <junanthony.business@gmail.com>
2025-07-31 23:45:16 +1200

    Add navigation

---
+++ app/index.jsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { Link } from 'expo-router'
+import logo from '../assets/img/refresh_light.png'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+
+      <Image source={logo} style={styles.logo} />
+      <Text style={[styles.title, {textDecorationLine: 'underline'}]}>Welcome Again!</Text>
+      <Text style={{ marginTop: 10, color:  'blue'}}>Reading list app</Text>
+
+      <View style={styles.card}>
+        <Text>Hello, this is a card.</Text>
+      </View>
+
+      <Link href="/about" style={styles.link}>About Page</Link>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)',
+        marginVertical: 50
+    },
+    logo: {
+        height: 80,
+        width: 80
+    },
+    link: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+        marginVertical: 20
+    }
+})
